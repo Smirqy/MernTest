177b0518999c1c1c505641119cd244932e40c49d
Smirqy <noahrk14@gmail.com>
2022-12-07 22:26:33 -0700

    create backend and start front end

---
+++ mern-test/backend/index.js
@@ -0,0 +1,28 @@
+//connect database and start server
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
+dotenv.config()
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS:2500,
+    }
+    )
+    .catch(err=> {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    //starts webserver
+    .then(async client => {
+        await RestaurantsDAO.injectDB(client)
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
